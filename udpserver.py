3175c2b7c654ad86c5ef15b8258202033898a4b7
Michael <lizicloud@gmail.com>
2017-04-18 08:59:18 +0800

    convert tabs to spaces

--- udpserver.py
+++ udpserver.py
@@ -26,36 +26,36 @@ def receive_signal(signum, stack):
 
 
 def main(port):
-	address = ('127.0.0.1', port)  
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-	s.bind(address)
-	try:
-		while True:  
-		    data, addr = s.recvfrom(2048)
-		    logger.info("received: %s , from %s " % (data, addr[0]))
-	except Exception,e:
-	    print e
-	s.close()  
+    address = ('127.0.0.1', port)  
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+    s.bind(address)
+    try:
+        while True:  
+            data, addr = s.recvfrom(2048)
+            logger.info("received: %s , from %s " % (data, addr[0]))
+    except Exception,e:
+        print e
+    s.close()  
 
 
 if __name__ == '__main__':
-	# Setup opts & args
-	list=50
-	port=30000
-	opts, args = getopt.getopt(sys.argv[1:], "hp:l:",["help","port=","list="])
-	for op, value in opts:
-	    if op in ("-l","--list"):
-	        list = string.atol(value)
-	    elif op in ("-p","--port"):
-	        port = string.atol(value)
-	    elif op in ("-h"):
-	        usage()
-	        sys.exit()
+    # Setup opts & args
+    list=50
+    port=30000
+    opts, args = getopt.getopt(sys.argv[1:], "hp:l:",["help","port=","list="])
+    for op, value in opts:
+        if op in ("-l","--list"):
+            list = string.atol(value)
+        elif op in ("-p","--port"):
+            port = string.atol(value)
+        elif op in ("-h"):
+            usage()
+            sys.exit()
 
-	logger.info('start udp server...')
-	# Setup signal
-	signal.signal(signal.SIGINT, receive_signal)
+    logger.info('start udp server...')
+    # Setup signal
+    signal.signal(signal.SIGINT, receive_signal)
 
-	main(port)
+    main(port)
 
 
